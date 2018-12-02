369ad40af5f16ac697b5434bbe11a69d81951371
bevi-rafael <bevi.rafael@gmail.com>
2018-12-01 22:42:42 -0200

    Desenvolvimento

--- webapp/controller/S1_MainScreen.controller.js
+++ webapp/controller/S1_MainScreen.controller.js
@@ -1,6 +1,7 @@
 sap.ui.define([
-	"com/arcelor/scm/ordertrack/controller/BaseController"
-], function(BaseController) {
+	"com/arcelor/scm/ordertrack/controller/BaseController",
+	'sap/ui/model/json/JSONModel'
+], function(BaseController, JSONModel) {
 	"use strict";
 
 	return BaseController.extend("com.arcelor.scm.ordertrack.controller.S1_MainScreen", {
@@ -17,19 +18,26 @@ sap.ui.define([
 		}, 
 		
 		onInit: function() {
-			this.getRouter().getRoute("mainScreen").attachMatched(this._routeMatched, this);
-			this.oVizBaseline = this.getView().byId('idVizBaseline');
+
+			//Data de exibição no cabeçalho
+			var oModel = new JSONModel();
+			oModel.setData({
+				dateValue: new Date()
+			});
+			this.getView().setModel(oModel);
+
+			//Retira o titulo dos donuts
 			this._initializeVizChart();	
-			
 		},
 		
 		onSelectDataBaseline : function(oEvent){
 			var aData = oEvent.getParameter('data');
-
+			var oSelectedObject = aData[0].data;
+			
 			var oVizBaseline 	= oEvent.getSource(); 
 			var oDataset 		= oVizBaseline.getDataset();
 			var oBinding 		= oDataset.getBinding('data');
-			var oContext 		= oBinding.getContext(aData[0]._contextRow);
+			var oContext 		= oBinding.getContexts()[oSelectedObject._context_row_number];
 			var oObject			= oContext.getObject(); 
 			
 			oEvent.getSource().vizSelection(
@@ -37,8 +45,35 @@ sap.ui.define([
 			);
 		}, 
 		
+		onSelectDataReplanejado : function(oEvent){
+			var aData = oEvent.getParameter('data');
+			var oSelectedObject = aData[0].data;
+			
+			var oVizBaseline 	= oEvent.getSource(); 
+			var oDataset 		= oVizBaseline.getDataset();
+			var oBinding 		= oDataset.getBinding('data');
+			var oContext 		= oBinding.getContexts()[oSelectedObject._context_row_number];
+			var oObject			= oContext.getObject(); 
+			
+			oEvent.getSource().vizSelection(
+				oEvent.getParameter('data'), { clearSelection: true }
+			);
+		}, 
+
+		_pressChart: function() {
+
+		}, 
+		
 		_initializeVizChart: function() {
+			this.getRouter().getRoute("mainScreen").attachMatched(this._routeMatched, this);
+			
+			//Donut BASELINE
+			this.oVizBaseline = this.getView().byId('idVizBaseline');
 			this.oVizBaseline.setVizProperties(this._mVizDonutProperties);
+			
+			//Donut REPLANEJADO
+			this.oVizReplanejado = this.getView().byId('idVizReplanejado');
+			this.oVizReplanejado.setVizProperties(this._mVizDonutProperties);
 		}, 
 		
 		_routeMatched : function(oEvent) {
